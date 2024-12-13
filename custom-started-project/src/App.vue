7eab3ff934f9003c81e8122934239c1725ae6497
lyklecharova <lklecharova9@gmail.com>
2024-12-13 16:44:44 +0200

    Update App.vue

--- custom-started-project/src/App.vue
+++ custom-started-project/src/App.vue
@@ -1,14 +1,41 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      numberOfClicks: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.numberOfClicks += 1;
+    },
+    decrement() {
+      this.numberOfClicks -= 1;
+    },
   },
 };
 </script>
 
 <template>
-  <h1>Hello world</h1>
-  <HelloWorld />
+  <h1 class="myTitle">
+    Hello World
+  </h1>
+  <h2>
+    the button was clicked
+    {{ numberOfClicks }}
+    number of times!
+  </h2>
+
+  <button @click="increment">
+    increment
+  </button>
+  <button @click="decrement">
+    decrement
+  </button>
 </template>
+
+<style>
+.myTitle{
+  color: red;
+}
+</style>
