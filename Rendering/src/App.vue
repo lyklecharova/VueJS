fa4a62841eba4ff37ef5aad43bc9ccc13d8da137
lyklecharova <lklecharova9@gmail.com>
2025-01-02 20:30:18 +0200

    add new

---
+++ Rendering/src/App.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  data() {
+    return {
+      isDay: true,
+    };
+  },
+  methods: {
+    onDayChange() {
+      this.isDay = !this.isDay;
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Hello world</h1>
+  <h2 v-if="isDay">
+    ☀️ It's sunny outside!
+  </h2>
+  <h2 v-else>
+    🌑 It's dark outside!
+  </h2>
+
+  <template v-if="isDay">
+    <p>
+      🎉 Welcome to My Awesome Page 🎉
+    </p>
+    <span>
+      👋 Hello there! This is a cool Vue.js example.
+    </span>
+  </template>
+  
+  <p v-else>
+    Click the button above to reveal the exciting content! 😄
+  </p>
+
+  <button @click="onDayChange">
+    Toggle time
+  </button>
+</template>
